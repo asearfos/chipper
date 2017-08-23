fc7e33e30729edc9a945ca10a9a3738abdedcfa4
asearfos <abigail.m.searfoss@gmail.com>
2017-08-23 17:01:36 -0500

    Added close button when hit end of files in directory. Added ScreenManager to have first screen as file manager and second screen as my control panel for segmenting songs. Still need to get FileBrowser working. Also have notes on where/how to improve the plotting speed and the writing dictionary to file.

--- SegmentSyllablesGUI_withJames.py
+++ SegmentSyllablesGUI_withJames.py
@@ -4,6 +4,14 @@ kivy.require('1.10.0')
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.slider import Slider
+from kivy.uix.popup import Popup
+from kivy.uix.screenmanager import ScreenManager, Screen
+
+#from kivy.uix.filechooser import FileChooser
+
+from os.path import sep, expanduser, isdir, dirname
+from kivy.garden.filebrowser import FileBrowser
+from kivy.utils import platform
 
 import matplotlib
 matplotlib.use("module://kivy.garden.matplotlib.backend_kivy")
@@ -15,18 +23,59 @@ import segmentSylls_functionsForGUI as seg
 import csv
 
 
-class ControlPanel(BoxLayout):
+class Manager(ScreenManager):
+    pass
 
-    def __init__(self):
-        super(ControlPanel, self).__init__()
+
+class ScreenOne(Screen):
+    def _fbrowser_canceled(self, instance):
+        print ('cancelled, Close self.')
+
+    def _fbrowser_success(self, instance):
+        print (instance.selection)
+    #
+    # def getBrowser(self):
+    #     if platform == 'win':
+    #         user_path = dirname(expanduser('~')) + sep + 'Documents'
+    #     else:
+    #         user_path = expanduser('~') + sep + 'Documents'
+    #     browser = FileBrowser(select_string='Select',
+    #                           favorites=[(user_path, 'Documents')])
+    #     browser.bind(
+    #                 on_success=self._fbrowser_success,
+    #                 on_canceled=self._fbrowser_canceled)
+    #     return browser
+
+
+class DonePopup(Popup):
+    pass
+
+
+class ControlPanel(Screen):
+    def __init__(self, directory, **kwargs):
+        super(ControlPanel, self).__init__(**kwargs)
+        self.directory = directory
+
+    def setup(self):
         self.i = 0
         self.directory = "C:/Users/abiga/Box Sync/Abigail_Nicole/TestingGUI/PracticeBouts/"
         # self.directory = "C:/Users/abiga/Box Sync/Abigail_Nicole/chipping sparrow new recording/fromEBird/eBird_MLCatNum_ChippingSparrows_asOf07142017_fromMatthewYoung/eBird_MLCatNum_ChippingSparrows_asOf07142017_fromMatthewYoung_bouts/"
         self.files = seg.initialize(self.directory)
         self.save_parameters = {}
         self.save_syllables = {}
-        self.tossed = {}
+        self.save_tossed = {}
         self.next()
+
+    # def __init__(self,**kwargs):
+    #     super(ControlPanel, self).__init__(**kwargs)
+    #     self.i = 0
+    #     self.directory = "C:/Users/abiga/Box Sync/Abigail_Nicole/TestingGUI/PracticeBouts/"
+    #     # self.directory = "C:/Users/abiga/Box Sync/Abigail_Nicole/chipping sparrow new recording/fromEBird/eBird_MLCatNum_ChippingSparrows_asOf07142017_fromMatthewYoung/eBird_MLCatNum_ChippingSparrows_asOf07142017_fromMatthewYoung_bouts/"
+    #     self.files = seg.initialize(self.directory)
+    #     self.save_parameters = {}
+    #     self.save_syllables = {}
+    #     self.save_tossed = {}
+    #     #self.next()
 
     def next(self):
         # set default parameters
@@ -35,56 +84,68 @@ class ControlPanel(BoxLayout):
         self.min_silence = 10
         self.min_syllable = 20
 
-        #connect defaults to .kv (would like to do this the other way around)
+        # connect defaults to .kv (would like to do this the other way around)
         self.ids.slider_high_pass_filter.value = self.filter_boundary
         self.ids.slider_threshold.value = self.percent_keep
         self.ids.slider_min_silence.value = self.min_silence
         self.ids.slider_min_syllable.value = self.min_syllable
 
         self.sonogram = seg.initial_sonogram(self.i, self.files, self.directory)
-        #run update to load images for the first time for this file
+        # run update to load images for the first time for this file
         self.syllable_onsets, self.syllable_offsets = self.update(self.sonogram, 513-self.filter_boundary, self.percent_keep, self.min_silence, self.min_syllable)
         self.i += 1
-        print(self.i)
 
     def save(self):
         # save parameters to dictionary
-        print(self.i-1)
         self.save_parameters[self.files[self.i-1]] = {'HighPassFilter': self.filter_boundary, 'PercentSignalKept': self.percent_keep, 'MinSilenceDuration': self.min_silence, 'MinSyllableDuration': self.min_syllable}
         self.save_syllables[self.files[self.i-1]] = {'Onsets': self.syllable_onsets, 'Offsets': self.syllable_offsets}
-        print(self.save_parameters)
         # go to next file
         if self.i == len(self.files):
-            # write save_parameters to files
-            with open((self.directory+'segmentedSyllables_parameters'), 'w') as params:
-                params_fields = ['FileName', 'HighPassFilter', 'PercentSignalKept', 'MinSilenceDuration', 'MinSyllableDuration']
-                params_file = csv.DictWriter(params, params_fields, delimiter='\t')
-                params_file.writeheader()
-                for key, val in self.save_parameters.items():
-                    row = {'FileName': key}
-                    row.update(val)
-                    params_file.writerow(row)
-                params.close()
-            with open((self.directory+'segmentedSyllables_syllables'), 'w') as sylls:
-                sylls_fields = ['FileName', 'Onsets', 'Offsets']
-                sylls_file = csv.DictWriter(sylls, sylls_fields, delimiter='\t')
-                sylls_file.writeheader()
-                for key, val in self.save_syllables.items():
-                    row = {'FileName': key}
-                    print(row)
-                    row.update(val)
-                    print(row)
-                    sylls_file.writerow(row)
-                sylls.close()
-            quit()
+            self.write()
         else:
             self.next()
 
     def toss(self):
         # send file name to .txt or send file to folder
-        self.tossed[self.i-1] = {self.files[self.i-1]}
-        print(self.tossed)
-        self.next()
+        self.save_tossed[self.i-1] = {'FileName': self.files[self.i-1]}
+        # print(self.save_tossed)
+        if self.i == len(self.files):
+            self.write()
+        else:
+            self.next()
+
+    def write(self):
+        # write save_parameters to files
+        with open((self.directory + 'segmentedSyllables_parameters'), 'w') as params:
+            params_fields = ['FileName', 'HighPassFilter', 'PercentSignalKept', 'MinSilenceDuration',
+                             'MinSyllableDuration']
+            params_file = csv.DictWriter(params, params_fields, delimiter='\t')
+            params_file.writeheader()
+            for key, val in self.save_parameters.items():
+                row = {'FileName': key}
+                row.update(val)
+                params_file.writerow(row)
+            params.close()
+        with open((self.directory + 'segmentedSyllables_syllables'), 'w') as sylls:
+            sylls_fields = ['FileName', 'Onsets', 'Offsets']
+            sylls_file = csv.DictWriter(sylls, sylls_fields, delimiter='\t')
+            sylls_file.writeheader()
+            for key, val in self.save_syllables.items():
+                row = {'FileName': key}
+                row.update(val)
+                sylls_file.writerow(row)
+            sylls.close()
+        # try using pandas dataframe --> to csv
+        with open((self.directory + 'segmentedSyllables_tossed'), 'w') as tossed:
+            tossed_fields = ['FileName']
+            tossed_file = csv.DictWriter(tossed, tossed_fields, delimiter='\t')
+            tossed_file.writeheader()
+            for key, val in self.save_tossed.items():
+                row = {'FileName': key}
+                row.update(val)
+                tossed_file.writerow(row)
+            tossed.close()
+        self.done_window()
 
     def update(self, sonogram, filter_boundary, percent_keep, min_silence, min_syllable):
         self.filter_boundary = filter_boundary
@@ -92,7 +153,7 @@ class ControlPanel(BoxLayout):
         self.min_silence = min_silence
         self.min_syllable = min_syllable
         #sonogram = seg.initial_sonogram(self.i, self.files, self.directory)
-
+        sonogram = self.sonogram.copy()
         hpf_sonogram = seg.high_pass_filter(filter_boundary, sonogram)
         scaled_sonogram = seg.normalize_amplitude(hpf_sonogram)
         self.image_sonogram(hpf_sonogram)
@@ -116,6 +177,7 @@ class ControlPanel(BoxLayout):
 
         fig1 = plt.gcf()
         self.ids.graph_sonogram.add_widget(FigureCanvasKivyAgg(fig1))
+        # look up how to speed up matplotlip --> make one canvas
         return plot_sonogram
 
     def image_binary(self, data, syllable_marks):
@@ -139,6 +201,9 @@ class ControlPanel(BoxLayout):
         self.ids.graph_binary.add_widget(FigureCanvasKivyAgg(fig2))
         return plot_sonogram
 
+    def done_window(self):
+        done_popup = DonePopup()
+        done_popup.open()
 
 class MySlider(Slider):
     def __init__(self, **kwargs):
@@ -157,7 +222,7 @@ class MySlider(Slider):
 
 class SegmentSyllablesGUI_withJamesApp(App):
     def build(self):
-        return ControlPanel()
+        return Manager()
 
 if __name__ == "__main__":
     SegmentSyllablesGUI_withJamesApp().run()
