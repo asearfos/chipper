3f38212d3ca3d4ec60851646e4ffa6a1316a03bf
asearfos <abigail.m.searfoss@gmail.com>
2017-10-13 18:11:30 -0500

    Continuing to compare with MATLAB - found problem where it was not getting the entire syllable when doing comparisons --> fixed this in how onsets and offsets are determined/defined by the GUI (so fix was not in this code) - still trying to figure out if stereotypy is calculating correctly (either Python or Matlab implementation is wrong)
    To Do:
    - still need to add sequential calculation
    - need to decide on how freq is binned
    - still unsure about note categorization
    - decide on which standard deviation is appropriate

--- SegmentSyllablesGUI/SegSylls_analysisForGUI.py
+++ SegmentSyllablesGUI/SegSylls_analysisForGUI.py
@@ -4,10 +4,11 @@ import gzip
 import pandas as pd
 import os
 import time
-# from scipy.ndimage.measurements import label
+from scipy.ndimage.measurements import label as label2
 from skimage.measure import label, regionprops
 from skimage.color import label2rgb
 from matplotlib import pyplot as plt
+import matplotlib.transforms as tx
 
 
 class SyllableAnalysis(object):
@@ -21,10 +22,19 @@ class SyllableAnalysis(object):
         # load data
         self.onsets, self.offsets, self.threshold_sonogram = self.load_bout_data(dirname, basename)
 
+        # fig, ax = plt.subplots()
+        # ax.imshow(self.threshold_sonogram)
+        # syllable_marks = np.append(self.onsets, self.offsets)
+        # ymin = np.zeros(len(syllable_marks))
+        # ymax = 513
+        # ax.vlines(syllable_marks, ymin=ymin, ymax=ymax, colors='m', linewidth=0.5)
+        # plt.show()
+        # # print('sonogram size', self.threshold_sonogram.shape)
+
         # run analysis
         syllable_durations, num_syllables, bout_stats = self.get_bout_stats()
         syllable_stats = self.get_syllable_stats(syllable_durations, num_syllables)
-        note_stats = self.get_note_stats()
+        note_stats = self.get_note_stats(num_syllables)
 
         # write output
         final_output = self.update_dict([bout_stats, syllable_stats, note_stats])
@@ -41,10 +51,6 @@ class SyllableAnalysis(object):
             for line in fin:
                 json_line = json.loads(line, encoding='utf-8')
                 song_data.append(json_line)
-                # json_bytes = fin.read()
-                # json_str = json_bytes.decode('utf-8')
-                # print(type(json_str))
-                # song_data = json.loads(json_bytes, encoding='utf-8')
             fin.close()
 
         onsets = np.asarray(song_data[1]['Onsets'], dtype='int')
@@ -87,8 +93,8 @@ class SyllableAnalysis(object):
         num_syllables_per_bout_duration = num_syllables/bout_duration
         song_stats = {'bout_duration': bout_duration, 'num_syllables': num_syllables,
                       'num_syllable_per_bout_duration': num_syllables_per_bout_duration}
-        basic_syllable_stats = self.get_basic_stats(syllable_durations, 'syllable')
-        basic_silence_stats = self.get_basic_stats(silence_durations, 'silence')
+        basic_syllable_stats = self.get_basic_stats(syllable_durations, 'syllable_duration')
+        basic_silence_stats = self.get_basic_stats(silence_durations, 'silence_duration')
         bout_stats = self.update_dict([song_stats, basic_syllable_stats, basic_silence_stats])
 
         return syllable_durations, num_syllables, bout_stats
@@ -116,7 +122,8 @@ class SyllableAnalysis(object):
 
                 maxoverlap = max(sonogram_self_correlation[j], sonogram_self_correlation[k])
 
-                shift_factor = abs(syllable_durations[j]-syllable_durations[k])
+                shift_factor = np.array(abs(syllable_durations[j]-syllable_durations[k]))
+                # shift_factor[shift_factor == 0] = 1
 
                 if syllable_durations[j] < syllable_durations[k]:
                     min_length = syllable_durations[j]
@@ -130,6 +137,7 @@ class SyllableAnalysis(object):
                 sonogram_correlation[k, j] = max(syllable_correlation)
 
         # print(np.shape(sonogram_correlation), (sonogram_correlation.transpose() == sonogram_correlation).all())
+        print(sonogram_correlation)
 
         sonogram_correlation_binary = np.zeros(sonogram_correlation.shape)
         sonogram_correlation_binary[sonogram_correlation > corr_thresh] = 1
@@ -137,10 +145,10 @@ class SyllableAnalysis(object):
 
     def get_syllable_correlation(self, a, b, shift_factor, min_length, maxoverlap):
         syllable_correlation = []
-        for m in range(shift_factor+1):
+        for m in range(shift_factor + 1):
             syll_1 = self.threshold_sonogram[:, self.onsets[a]:(self.onsets[a] + min_length)]
             syll_2 = self.threshold_sonogram[:, (self.onsets[b] + m):(self.onsets[b] + min_length + m)]
-            syllable_correlation.append((sum(sum(syll_1 * syll_2)) / maxoverlap) * 100)
+            syllable_correlation.append((sum(sum(syll_1*syll_2))/maxoverlap)*100)
         return syllable_correlation
 
     def get_syllable_stats(self, syllable_durations, num_syllables, corr_thresh=50):
@@ -169,6 +177,7 @@ class SyllableAnalysis(object):
 
         # determine syllable stereotypy
         syllable_stereotypy = np.zeros(len(sonogram_correlation))
+
         for j in range(len(sonogram_correlation)):
             x_syllable_locations = np.where(syllable_pattern_checked == j)[0]  # locations of all like syllables
             # initialize arrays
@@ -179,11 +188,20 @@ class SyllableAnalysis(object):
                         if k > h:  # fill only the lower triangle (not upper and not diagonal) so that similarities aren't double counted when taking the mean later
                             x_syllable_correlations[k, h] = sonogram_correlation[x_syllable_locations[k],
                                                                                  x_syllable_locations[h]]
-            syllable_stereotypy[j] = np.mean(x_syllable_correlations[x_syllable_correlations != 0])
+
+            if j == 11:
+                print(x_syllable_locations)
+                print(x_syllable_correlations)
+
+            syllable_stereotypy[j] = np.nanmean(x_syllable_correlations[x_syllable_correlations != 0])
+
+        mean_syllable_stereotypy = np.nanmean(syllable_stereotypy)
+        std_syllable_stereotypy = np.nanstd(syllable_stereotypy)
 
         syllable_stats = {'num_unique_syllables': num_unique_syllables, 'num_syllables_per_num_unique':
-            num_syllables_per_num_unique, 'syllable_pattern': syllable_pattern_checked, 'syllable_stereotypy':
-            syllable_stereotypy}
+            num_syllables_per_num_unique, 'syllable_pattern': syllable_pattern_checked,
+                          'syllable_stereotypy': syllable_stereotypy, 'mean_syllable_stereotypy':
+            mean_syllable_stereotypy, 'std_syllable_stereotypy': std_syllable_stereotypy}
         return syllable_stats
 
     """
@@ -194,12 +212,20 @@ class SyllableAnalysis(object):
                                                                         # ^connectivity 1=4 or 2=8(include diagonals)
         props = regionprops(labeled_sonogram)
 
-        image_label_overlay = label2rgb(labeled_sonogram, image=self.threshold_sonogram)
-        imgplot = plt.imshow(labeled_sonogram)
-        # imgplot = plt.imshow(self.threshold_sonogram)
-        plt.colorbar()
-        # imgplot.set_cmap('nipy_spectral')
-        plt.show()
+        # labeled_sonogram2, num_notes2 = label2(self.threshold_sonogram)
+
+        # # image_label_overlay = label2rgb(labeled_sonogram, image=self.threshold_sonogram)
+        # imgplot = plt.imshow(labeled_sonogram, cmap='spectral')
+        # # imgplot = plt.imshow(self.threshold_sonogram)
+        # plt.colorbar()
+        # # imgplot.set_cmap('nipy_spectral')
+        # plt.show()
+        #
+        # plt.imshow(labeled_sonogram2)
+        # plt.colorbar()
+        # plt.show()
+        # plt.title('ndimage')
+
         return num_notes, props
 
     def convert_freq(self, freq_range_upper, freq_range_lower, freq_factor=22050/513):
@@ -214,12 +240,11 @@ class SyllableAnalysis(object):
         mean_freq_modulation = np.mean(freq_modulation_per_note)
         std_freq_modulation = np.std(freq_modulation_per_note)
         freq_dict = {'avg_upper_freq': avg_upper_freq, 'avg_lower_freq': avg_lower_freq, 'max_freq': max_freq,
-                     'min_freq': min_freq, 'overall_freq_range': overall_freq_range, 'freq_modulation_per_note':
-                     freq_modulation_per_note, 'mean_freq_modulation': mean_freq_modulation,
-                     'std_freq_modulation': std_freq_modulation}
+                     'min_freq': min_freq, 'overall_freq_range': overall_freq_range, 'mean_freq_modulation':
+                         mean_freq_modulation, 'std_freq_modulation': std_freq_modulation}
         return freq_dict
 
-    def get_note_stats(self):
+    def get_note_stats(self, num_syllables):
         num_notes, props = self.get_notes()
         num_notes_updated = num_notes  # initialize, will be altered if the "note" is too small (<60 pixels)
 
@@ -237,17 +262,21 @@ class SyllableAnalysis(object):
         for j in range(num_notes):
             note_ycoords = []  # clear/initialize for each note
 
-            # use bounding box of the note
-            min_row, min_col, max_row, max_col = props[j].bbox
-            freq_range_upper.append(min_row)
-            freq_range_lower.append(max_row - 1)
-
             # use only the part of the matrix with the note
             sonogram_one_note = props[j].filled_image
+
             if np.size(sonogram_one_note) <= 60:
                 note_length.append(0)  # place holder
                 num_notes_updated -= 1
+                # print('not a note')
             else:  # check the note is actually large enough to be a note and not just noise
+
+                # use bounding box of the note (note, indexing is altered since python starts with 0 and we want to
+                # convert rows to actual frequency)
+                min_row, min_col, max_row, max_col = props[j].bbox
+                freq_range_upper.append(min_row + 1)
+                freq_range_lower.append(max_row)
+
                 note_length.append(np.shape(sonogram_one_note)[1])
 
                 # fit quadratic to notes
@@ -258,16 +287,30 @@ class SyllableAnalysis(object):
                 a = poly[0]
                 b = poly[1]
                 x_vertex = -b/(2*a)  # gives x position of max or min of quadratic
+                # print('original', poly)
 
-                print(poly)
+                # # new package
+                # import numpy.polynomial.polynomial as poly
+                # coefs = poly.polyfit(note_xcoords, note_ycoords, 2)
+                # # print('new poly', coefs)
+                # x_new = np.linspace(note_xcoords[0], note_xcoords[-1], num=len(note_xcoords)*10)
+                # ffit = poly.polyval(x_new, coefs)
+                # # plt.scatter(note_xcoords, note_ycoords)
+                # # plt.plot(x_new, ffit)
+                # # plt.imshow(sonogram_one_note)
+                # # plt.show()
+
                 # classify shape of note
-                if np.isclose(a, 0):  # check if the note is linear
-                    if np.isclose(b, 0):
+                if np.isclose(a, 0, rtol=9e-02, atol=9e-02):  # check if the note is linear
+                    if np.isclose(b, 0, rtol=9e-02, atol=9e-02):
                         num_flat += 1
+                        # print('is flat')
                     elif b > 0:  # b is the slope if the poly is actually linear
                         num_upsweeps += 1
+                        # print('is upsweep')
                     else:
                         num_downsweeps += 1
+                        # print('is downsweep')
                 # now categorize non-linear notes
                 elif x_vertex < .2*note_length[j]:
                     if a > 0:
@@ -283,21 +326,27 @@ class SyllableAnalysis(object):
                     num_parabolas += 1
 
         # collect stats into dictionaries for output
-        basic_stats = self.get_basic_stats(note_length, 'note_length')
-        note_categories = {'num_notes': num_notes_updated, 'num_flat': num_flat, 'num_upsweeps': num_upsweeps,
-                           'num_downsweeps': num_downsweeps,
-                           'num_parabolas': num_parabolas}
+        note_length_array = np.asarray(note_length)
+        note_info = {'num_notes': num_notes_updated, 'num_notes_per_syll': num_notes_updated/num_syllables}
+        basic_stats = self.get_basic_stats(note_length_array[note_length_array != 0], 'note_duration')
+        # note_categories = {'num_flat': num_flat/num_notes_updated, 'num_upsweeps': num_upsweeps/num_notes_updated,
+        #                    'num_downsweeps': num_downsweeps/num_notes_updated, 'num_parabolas':
+        #                        num_parabolas/num_notes_updated}
+        note_categories = {'num_flat': num_flat, 'num_upsweeps': num_upsweeps,
+                           'num_downsweeps': num_downsweeps, 'num_parabolas':
+                               num_parabolas}
         freq_stats = self.convert_freq(freq_range_upper, freq_range_lower)
 
-        note_stats = self.update_dict([basic_stats, note_categories, freq_stats])
+        note_stats = self.update_dict([note_info, basic_stats, note_categories, freq_stats])
         return note_stats
 
 
-
 # part that actually calls/runs code
-filepath = 'C:/Users/abiga\Box Sync\Abigail_Nicole\TestingGUI\white crowned sparrows for ' \
-           'testing\OneBout\Output_20171004_T141722\output_b1s white crowned sparrow 66722amp.gzip'
-SyllableAnalysis(filepath)
+# filepath = 'C:/Users/abiga\Box Sync\Abigail_Nicole\TestingGUI\white crowned sparrows for ' \
+#            'testing\OneBout\Output_20171004_T141722\output_b1s white crowned sparrow 66722amp.gzip'
+filepath_newOnOff = 'C:/Users/abiga\Box Sync\Abigail_Nicole\TestingGUI\white crowned sparrows for ' \
+                    'testing\OneBout\SeqSyllsOutput_20171013_T164506\SegSyllsOutput_b1s white crowned sparrow 66722amp.gzip'
+SyllableAnalysis(filepath_newOnOff)
 
 
 
