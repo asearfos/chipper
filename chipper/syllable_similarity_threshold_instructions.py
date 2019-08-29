68d922073c89b016b44944845e7e06c0b8bf24f9
asearfos <asearfos@gmail.com>
2019-08-29 18:27:16 -0500

    Updated Syllable Similarity instructions as well.

--- chipper/syllable_similarity_threshold_instructions.py
+++ chipper/syllable_similarity_threshold_instructions.py
@@ -7,7 +7,16 @@ text = 'The colors are here to help you distinguish the syntax of the song which
        'same as both first and second syllables.\n\n' \
        'To help, the average, minimum, and maximum percent similarity between like syllables is also provided. ' \
        'Note, the minimum can be less than the threshold due to the sequential nature of how the syntax is found.\n\n' \
-       'The goal is to determine a common threshold for syllable similarity for all of your data. ' \
-       'You will adjust the threshold for each song until satisfied with the results. ' \
-       'A summary of the thresholds used for all songs will be given at the end. ' \
-       'Then, you will be given the chance to adjust the final threshold to be used in song analysis.' \
+       'The purpose of the widget is to help you determine a common ' \
+       'threshold for syllable similarity for all of your data. ' \
+       'We recommend you perform this step for a set of songs from the same ' \
+       'species. Specifically, you can use a subset of your data (~20 songs) ' \
+       'to determine the threshold. You will adjust the threshold for each ' \
+       'song until satisfied with the results. A summary of the thresholds ' \
+       'used for the sample songs will be given at the end. Then, you will be ' \
+       'given the chance to adjust the final threshold to be used in song ' \
+       'analysis.\n\n' \
+       'Any signal between syllables appears grey and will not be ' \
+       'considered in the analysis. Similarly, any noise (determined using ' \
+       'the Noise Threshold from the previous step) will appear white and ' \
+       'will not be considered in the analysis.'
